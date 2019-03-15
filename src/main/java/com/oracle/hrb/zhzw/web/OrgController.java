9b8bf37e31454e732756a3ca932e59959afd0134
williamlee <williamlee@yeah.net>
2019-03-15 13:52:47 +0800

    下载数据为电子表格

--- src/main/java/com/oracle/hrb/zhzw/web/OrgController.java
+++ src/main/java/com/oracle/hrb/zhzw/web/OrgController.java
@@ -5,16 +5,19 @@ import com.github.pagehelper.PageInfo;
 import com.oracle.hrb.zhzw.bean.Org;
 import com.oracle.hrb.zhzw.bean.Result;
 import com.oracle.hrb.zhzw.service.OrgService;
+import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URLEncoder;
 import java.util.List;
 
-import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @RestController
 @RequestMapping("/org")
@@ -55,6 +58,58 @@ public class OrgController {
         Result r = new Result();
         orgService.deleteOrg(id);
         return r;
+    }
+    @RequestMapping(method = GET, value = "/sheet")
+    public void dowloadSheet(HttpServletResponse response) throws Exception {
+        HSSFWorkbook wb = new HSSFWorkbook();
+        HSSFSheet sheet = wb.createSheet("组织机构表");
+        // 设置列宽
+        sheet.setColumnWidth(0, 4000);
+        sheet.setColumnWidth(1, 4000);
+        sheet.setColumnWidth(2, 4000);
+        sheet.setColumnWidth(3, 4000);
+        sheet.setColumnWidth(4, 4000);
+
+        // 设置表头样式
+        HSSFCellStyle cellstyle = (HSSFCellStyle) wb.createCellStyle();
+        cellstyle.setAlignment(HorizontalAlignment.CENTER);// 设置居中
+        HSSFFont font = wb.createFont();
+        font.setBold(true);
+        font.setFontName("黑体");
+        font.setFontHeightInPoints((short)20);
+        cellstyle.setFont(font);
+        // 合并单元格
+        CellRangeAddress rangeAddress = new CellRangeAddress(0,0,0,4);
+        sheet.addMergedRegion(rangeAddress);
+
+        HSSFRow title = sheet.createRow(0);
+        HSSFCell cell = title.createCell(0);
+        cell.setCellStyle(cellstyle);
+        cell.setCellValue("组织机构表");
+
+        // 创建表头
+        HSSFRow head = sheet.createRow(1);
+        head.createCell(0).setCellValue("组织机构代码");
+        head.createCell(1).setCellValue("组织机构名称");
+        head.createCell(2).setCellValue("组织机构地址");
+        head.createCell(3).setCellValue("法人");
+        head.createCell(4).setCellValue("联系电话");
+        // 添加数据
+        List<Org> orgs = orgService.findAll();
+        for(int i = 0; i < orgs.size(); i++) {
+            Org org = orgs.get(i);
+            HSSFRow row = sheet.createRow(i + 2);
+            row.createCell(0).setCellValue(org.getCode());
+            row.createCell(1).setCellValue(org.getName());
+            row.createCell(2).setCellValue(org.getAddress());
+            row.createCell(3).setCellValue(org.getLegal());
+            row.createCell(4).setCellValue(org.getPhone());
+        }
+        // 发送生成的电子表格
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-Disposition", "attachment;filename="+URLEncoder.encode("组织机构表.xls", "UTF-8"));
+        wb.write(response.getOutputStream());
+        wb.close();
     }
 
 }
